7dc1b85387e1bad9e62c0d9c28213ae06a6581a7
obstinacy26 <obstinacy26@hotmail>
2018-05-09 15:56:46 +0800

    Initial commit

---
+++ src/Property/StatisticsDisplay.java
@@ -0,0 +1,17 @@
+package Property;
+
+import Display.DisplayElement;
+import Observer.Observer;
+
+public class StatisticsDisplay implements Observer, DisplayElement {
+
+    @Override
+    public void display() {
+
+    }
+
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+
+    }
+}
---
+++ src/Property/ThirdPartyDisplay.java
@@ -0,0 +1,16 @@
+package Property;
+
+import Display.DisplayElement;
+import Observer.Observer;
+
+public class ThirdPartyDisplay implements Observer, DisplayElement {
+    @Override
+    public void display() {
+
+    }
+
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+
+    }
+}
